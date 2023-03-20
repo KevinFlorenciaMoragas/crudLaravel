9aa32947b63400c2cd04b1a4d4500492f5748311
KevinFlorenciaMoragas <120371012+KevinFlorenciaMoragas@users.noreply.github.com>
2023-03-20 18:38:59 +0100

    20/03

---
+++ resources/views/about.blade.php
@@ -0,0 +1,13 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('About') }} Kevin Florencia
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <p>Soy Kevin Florencia estudiante de 2DAW</p>
+        </div>
+    </div>
+</x-app-layout>
